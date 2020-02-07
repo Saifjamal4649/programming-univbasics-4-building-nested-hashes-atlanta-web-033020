40bdf538958a2d852e46f5a52aa37961a2e29676
Saif Jamal <saifjamal4649@gmail.com>
2020-02-07 03:29:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,8 +29,8 @@ def monopoly_with_third_tier
   monopoly =  {
   :railroads =>
   {
-    :pieces=>4,
-    :rent_in_dollars=>
+    :pieces => 4,
+    :rent_in_dollars =>
     {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
@@ -53,7 +53,7 @@ def monopoly_with_fourth_tier
   # complexity.monopoly =  {
   :railroads =>
   {
-    :pieces=>4,
+    :pieces => 4,
     :rent_in_dollars =>
     {
       :one_piece_owned=>25,
